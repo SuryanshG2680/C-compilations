1d11f4576cb741c5697ac8700dc2db05da857ca4
Shinchan <suryanshg2680@gmail.com>
2025-10-13 14:28:23 +0530

    Update 16primeornot.c

--- 16primeornot.c
+++ 16primeornot.c
@@ -2,7 +2,22 @@
 
 int main(){
     int N, k;
+    int c=0;
      printf("PRIME OR COMPOSITE\n Enter the number to be checked:");
     scanf("%d",&N);
-    
+    for (int i=2; i<N; i++){
+        if (N%i==0){
+            
+            c++;
+        
+        }
+    }
+
+    if(c==0){
+        printf("%d is prime.",N);
+    }
+    else{
+        printf("%d is not a prime number.",N);
+    }
+    return 0;
 }
